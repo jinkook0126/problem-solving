50158bec97c861d11fca2e587b0a64f3a1c23a30
jinkook <jinqook0126@naver.com>
2023-04-07 12:47:55 +0900

    feat: 백준 10815

---
+++ baekjoon/10815.js
@@ -0,0 +1,45 @@
+/*
+https://www.acmicpc.net/problem/10815
+
+문제
+숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 
+정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 가지고 있는지 아닌지를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 
+둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다. 
+두 숫자 카드에 같은 수가 적혀있는 경우는 없다.
+셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 
+넷째 줄에는 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다
+
+출력
+첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 가지고 있으면 1을, 아니면 0을 공백으로 구분해 출력한다.
+
+비고
+ */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(line.trim());
+}).on("close", function () {
+  const N = input.shift();
+  const arr = input.shift().split(" ");
+  const M = input.shift();
+  const arr2 = input.shift().split(" ");
+  const map = new Map();
+  const res = [];
+
+  for (const x of arr) {
+    map.set(x, 1);
+  }
+
+  for (const x of arr2) {
+    res.push(map.get(x) || 0);
+  }
+  console.log(res.join(" "));
+  process.exit();
+});
