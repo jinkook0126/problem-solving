a76a78e7968ee2c190a5b514362550502d7eaf44
jinkook <jinqook0126@naver.com>
2023-04-10 15:17:08 +0900

    feat: 백준 10816

---
+++ baekjoon/10816.js
@@ -0,0 +1,49 @@
+/*
+https://www.acmicpc.net/problem/10816
+
+문제
+숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 
+정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 
+둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 
+넷째 줄에는 상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+
+출력
+첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 몇 개 가지고 있는지를 공백으로 구분해 출력한다.
+
+비고
+ */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(line.trim());
+}).on("close", function () {
+  const res = [];
+  const N = parseInt(input.shift());
+  const haveList = input
+    .shift()
+    .split(" ")
+    .map((x) => +x);
+  const M = parseInt(input.shift());
+  const findList = input
+    .shift()
+    .split(" ")
+    .map((x) => +x);
+  const map = new Map();
+  haveList.forEach((x) => {
+    map.set(x, map.get(x) + 1 || 1);
+  });
+  findList.forEach((x) => {
+    res.push(map.get(x) || 0);
+  });
+  console.log(res.join(" "));
+
+  process.exit();
+});
