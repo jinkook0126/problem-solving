6d93edcae649ed3711d8742df397821240c58a11
jinkook <jinqook0126@naver.com>
2023-04-11 17:27:59 +0900

    feat: 백준 2920

---
+++ baekjoon/2920.js
@@ -0,0 +1,35 @@
+/*
+https://www.acmicpc.net/problem/2920
+
+문제
+다장조는 c d e f g a b C, 총 8개 음으로 이루어져있다. 이 문제에서 8개 음은 다음과 같이 숫자로 바꾸어 표현한다. c는 1로, d는 2로, ..., C를 8로 바꾼다.
+1부터 8까지 차례대로 연주한다면 ascending, 8부터 1까지 차례대로 연주한다면 descending, 둘 다 아니라면 mixed 이다.
+연주한 순서가 주어졌을 때, 이것이 ascending인지, descending인지, 아니면 mixed인지 판별하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 8개 숫자가 주어진다. 이 숫자는 문제 설명에서 설명한 음이며, 1부터 8까지 숫자가 한 번씩 등장한다.
+
+출력
+첫째 줄에 ascending, descending, mixed 중 하나를 출력한다.
+
+비고
+ */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(line.trim());
+}).on("close", function () {
+  const str = input.shift().replaceAll(" ", "");
+  if (str === "12345678") {
+    console.log("ascending");
+  } else if (str === "87654321") {
+    console.log("descending");
+  } else {
+    console.log("mixed");
+  }
+  process.exit();
+});
