8d7093af4757878220f93d384555dd49827b50e9
jinkook <jinqook0126@naver.com>
2023-04-11 18:00:19 +0900

    feat: 백준 2475, 2577, 2741, 10871

---
+++ baekjoon/2741.js
@@ -0,0 +1,30 @@
+/*
+https://www.acmicpc.net/problem/2741
+
+문제
+자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+
+ */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(parseInt(line.trim()));
+}).on("close", function () {
+  const N = input.shift();
+  const res = [];
+  for (let i = 1; i <= N; i++) {
+    res.push(i);
+  }
+  console.log(res.join("\n"));
+  process.exit();
+});
