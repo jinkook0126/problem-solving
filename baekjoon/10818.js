1264fb79876deb964be28586a4750be0ec69a3bc
jinkook <jinqook0126@naver.com>
2023-04-11 17:17:42 +0900

    feat: 백준 10818

---
+++ baekjoon/10818.js
@@ -0,0 +1,29 @@
+/*
+https://www.acmicpc.net/problem/10818
+
+문제
+N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 
+모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+출력
+첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+
+비고
+ */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(line.trim());
+}).on("close", function () {
+  const N = input.shift();
+  const arr = input.shift().split(" ");
+  console.log(`${Math.min(...arr)} ${Math.max(...arr)}`);
+  process.exit();
+});
