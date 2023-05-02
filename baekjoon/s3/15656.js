25cb66d5c49175791fe3039c26a0b1d7aab8e55b
jinkook <jinqook0126@naver.com>
2023-05-02 15:35:41 +0900

    feat: 백준 15656   - dfs

---
+++ baekjoon/s3/15656.js
@@ -0,0 +1,35 @@
+/*
+https://www.acmicpc.net/problem/15656
+ */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(
+    line
+      .trim()
+      .split(" ")
+      .map((x) => +x)
+  );
+}).on("close", function () {
+  const [N, M] = input.shift();
+  const arr = input.shift().sort((a, b) => a - b);
+  const tmp = [];
+  const ans = [];
+  const DFS = (L) => {
+    if (L === M) {
+      ans.push(tmp.slice());
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        tmp[L] = arr[i];
+        DFS(L + 1);
+      }
+    }
+  };
+  DFS(0);
+  console.log(ans.map((x) => x.join(" ")).join("\n"));
+  process.exit();
+});
