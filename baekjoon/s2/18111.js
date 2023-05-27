5b544e84534eb928ff788cb5bd5ec7403cef755d
이진국 <jinkook0126@naver.com>
2023-05-28 00:12:06 +0900

    feat: 18111 진행 중

---
+++ baekjoon/s2/18111.js
@@ -0,0 +1,14 @@
+/*
+https://www.acmicpc.net/problem/18111
+ */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(line.trim());
+}).on("close", function () {
+  process.exit();
+});
