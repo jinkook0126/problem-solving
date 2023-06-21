b77bf55de32d34af636c3208480229bedaf41cd4
이진국 <jinkook0126@naver.com>
2023-06-22 01:28:41 +0900

    feat: 11053 - dp - S2

---
+++ baekjoon/dynamic_programming/11053.js
@@ -0,0 +1,23 @@
+// S2
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(line.trim());
+}).on("close", function () {
+  const N = parseInt(input[0]);
+  const arr = input[1].split(" ").map((x) => +x);
+  const dp = Array(N).fill(1);
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  console.log(Math.max(...dp));
+  process.exit();
+});
