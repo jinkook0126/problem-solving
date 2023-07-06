3043b197a95c46e666c21e39a499d7cacb9d4ea8
jinkook <jinqook0126@naver.com>
2023-07-06 17:58:01 +0900

    feat: 10799 - stack - S2

---
+++ baekjoon/data_structure/10799.js
@@ -0,0 +1,32 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(line.trim());
+}).on("close", function () {
+  const arr = input[0].split("");
+  const stack = [];
+  let flag = false;
+  let ans = 0;
+  arr.forEach((x) => {
+    if (x === "(") {
+      flag = true;
+      stack.push(x);
+    } else {
+      stack.pop();
+      if (flag) {
+        // 레이저 일 때
+        ans += stack.length;
+      } else {
+        // 쉬기둥일때
+        ans += 1;
+      }
+      flag = false;
+    }
+  });
+  console.log(ans);
+  process.exit();
+});
