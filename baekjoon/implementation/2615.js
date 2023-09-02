f7fbf6ce7d21dc9374ac2190770ef0b1d30ea6ce
이진국 <jinkook0126@naver.com>
2023-09-02 21:21:59 +0900

    feat: 2615 진행중

---
+++ baekjoon/implementation/2615.js
@@ -0,0 +1,88 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(
+    line
+      .trim()
+      .split(" ")
+      .map((x) => +x)
+  );
+}).on("close", function () {
+  let win = null;
+  let coordinate = [];
+  const dx = [-1, -1, 0, 1];
+  const dy = [0, 1, 1, 1];
+  for (let i = 0; i < 19; i++) {
+    for (let j = 0; j < 19; j++) {
+      if (input[i][j] === 1 || input[i][j] === 2) {
+        for (let k = 0; k < 8; k++) {
+          let left = true;
+          let right = true;
+          let num = 1;
+          const tmpCoor = [];
+          let cnt = 1;
+          while (left || right) {
+            let tx = i + dx[k] * num;
+            let ty = j + dy[k] * num;
+            if (
+              right &&
+              tx >= 0 &&
+              tx < 19 &&
+              ty >= 0 &&
+              ty < 19 &&
+              input[tx][ty] === input[i][j]
+            ) {
+              cnt += 1;
+              tmpCoor.push([tx, ty]);
+            } else {
+              right = false;
+            }
+            tx = i - dx[k] * num;
+            ty = j - dy[k] * num;
+
+            if (
+              left &&
+              tx >= 0 &&
+              tx < 19 &&
+              ty >= 0 &&
+              ty < 19 &&
+              input[tx][ty] === input[i][j]
+            ) {
+              cnt += 1;
+              tmpCoor.push([tx, ty]);
+            } else {
+              left = false;
+            }
+            num += 1;
+          }
+          if (cnt === 5) {
+            win = input[i][j];
+            tmpCoor.sort((a, b) => {
+              if (a[0] === b[0]) {
+                return a[1] - b[1];
+              } else {
+                return a[0] - b[0];
+              }
+            });
+            coordinate = [tmpCoor[0][0] + 1, tmpCoor[0][1] + 1];
+            break;
+          } else if (cnt > 5) {
+            win = null;
+          }
+        }
+      }
+      // if (win !== null) break;
+    }
+    // if (win !== null) break;
+  }
+  if (win === null) {
+    console.log(0);
+  } else {
+    console.log([win, coordinate.join(" ")].join("\n"));
+  }
+  process.exit();
+});
