7c40b530ec0d99e5208a83a5ebb252345d7eb7a2
jinkook <jinqook0126@naver.com>
2023-09-22 16:56:19 +0900

    feat: 21736 진행 중

---
+++ baekjoon/graph_dfs_bfs_backtracking/21736.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", function (line) {
+  input.push(line.trim());
+}).on("close", function () {
+  const [N, M] = input
+    .shift()
+    .split(" ")
+    .map((x) => +x);
+  const board = input.map((x) => x.split(""));
+  process.exit();
+});
